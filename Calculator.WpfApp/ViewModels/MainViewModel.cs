b9d307d4c852fc582ba140741903e8f698d45d1d
Karol Czuba <cz.karo@gmail.com>
2023-08-25 22:04:49 +0200

    Screen Value from code

---
+++ Calculator.WpfApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.WpfApp.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel() 
+        {
+            ScreenVal = "0";
+        }
+
+        private string _screenVal;
+
+        public string ScreenVal
+        {
+            get { return _screenVal; }
+            set 
+            {
+                _screenVal = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+  
--- Calculator.WpfApp/Views/MainWindow.xaml.cs
+++ Calculator.WpfApp/Views/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Calculator.WpfApp.ViewModels;
 using MahApps.Metro.Controls;
 
 namespace Calculator.WpfApp
@@ -24,6 +25,7 @@ namespace Calculator.WpfApp
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
         }
     }
 }
